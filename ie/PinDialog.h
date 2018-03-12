79e175fb14fd22d13b6906793dd6143cc724a2b4
Raul Metsma <raul@metsma.ee>
2018-03-12 09:24:49 +0200

    PINDialog cleanup and remove MFC (#60)
    Signed-off-by: Raul Metsma <raul@metsma.ee>
    # Conflicts:
    #	ie/PinDialog.cpp
    #	ie/PinDialog.h
    #	ie/esteid-plugin-ie.rc
    #	ie/resource.h

--- ie/PinDialog.h
+++ ie/PinDialog.h
@@ -18,76 +18,17 @@
 
 #pragma once
 
-#include "Logger.h"
-#include "resource.h"
-
-#include <atlbase.h>
-#include <atlhost.h>
-#include <atlstr.h>
-#include <atlctl.h>
+#include "stdafx.h"
 #include <string>
 
-class PinDialog : public CAxDialogImpl<PinDialog>
+class PinDialog
 {
 public:
-	PinDialog(const std::wstring &_label) : label(_label) {}
-	char* getPin();
-	void setAttemptsRemaining(int attemptsRemaining);
-	void setInvalidPin(bool wasPinInvalid);
+	static std::string getPin(const std::wstring &label, HWND pParent = NULL);
 
-	// Dialog Data
-	enum { IDD = IDD_PIN_DIALOG };
-
-protected:
-	BEGIN_MSG_MAP(PinDialog)
-		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
-		COMMAND_HANDLER(IDOK, BN_CLICKED, OnClickedOK)
-		COMMAND_HANDLER(IDCANCEL, BN_CLICKED, OnClickedCancel)
-		CHAIN_MSG_MAP(CAxDialogImpl<PinDialog>)
-	END_MSG_MAP()
-
-	LRESULT OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	LRESULT OnClickedOK(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
-	LRESULT OnClickedCancel(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
-
-	LRESULT OnCtlColorStatic(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled) {	
-		if (GetDlgItem(IDC_PIN_MESSAGE).m_hWnd == (HWND)lParam && (invalidPin || attemptsRemaining < 3)) {
-			SetTextColor((HDC)wParam, RGB(255, 0, 0));
-		}
-
-		SetBkColor((HDC)wParam, RGB(240, 240, 240));
-
-		HBRUSH  hBr = (HBRUSH)CreateSolidBrush(RGB(240, 240, 240));
-		return (LRESULT)hBr;
-	}
-
-  void updateControls() {
-    GotoDlgCtrl(GetDlgItem(IDC_PIN_FIELD));
-    if (invalidPin || attemptsRemaining < 3) {
-      std::wstring msg = getWrongPinErrorMessage();
-      _log("Sul on %i katset jäänud", attemptsRemaining);
-      SetDlgItemText(IDC_PIN_MESSAGE, &msg[0]);
-    }
-
-	SetDlgItemText(IDC_PIN_FIELD, L"");
-
-    // Disable text field and OK button of no more tries left. Set the focus to Cancel button.
-    if (attemptsRemaining <= 0) {
-	  GetDlgItem(IDC_PIN_FIELD).EnableWindow(FALSE);
-      GetDlgItem(IDOK).EnableWindow(FALSE);
-      GetDlgItem(IDCANCEL).SetFocus();
-    }
-
-    // make the window topmost and set focus to it
-    SetForegroundWindow(m_hWnd);
-  }
-
-protected:
-  static int attemptsRemaining;
-  static bool invalidPin;
-  char* pin;
-  int pinLen;
-  std::wstring label;
-  std::wstring getWrongPinErrorMessage();
-  std::wstring getEmptyPinErrorMessage();
+private:
+	PinDialog() {}
+	static INT_PTR CALLBACK DlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	std::wstring label;
+	std::string pin;
 };
--- ie/resource.h
+++ ie/resource.h
@@ -5,34 +5,3 @@
 #define IDR_ESTEIDPLUGINIE              101
 #define IDR_ESTEIDIEPLUGINBHO           102
 #define IDR_ESTEIDCERTIFICATE           103
-#define IDD_PIN_DIALOG                  104
-#define IDC_PIN_FIELD                   105
-#define IDC_PIN_MESSAGE                 106
-#define IDD_PROGRESSBARDLG              110
-#define IDC_PROGRESS_TEXT               1005
-#define IDC_PROGRESS_BAR                1006
-#define IDC_ERROR                       8004
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        204
-#define _APS_NEXT_COMMAND_VALUE         32768
-#define _APS_NEXT_CONTROL_VALUE         213
-#define _APS_NEXT_SYMED_VALUE           110
-#endif
-#endif
-
-#ifndef MAJOR_VER
-#define MAJOR_VER 1
-#endif
-#ifndef MINOR_VER
-#define MINOR_VER 2
-#endif
-#ifndef RELEASE_VER
-#define RELEASE_VER 0
-#endif
-#ifndef BUILD_VER
-#define BUILD_VER 0
-#endif
